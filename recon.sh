1182fc3d721077b2839d29a8f4fa6a7ff187caca
root <root@localhost.localdomain>
2018-06-20 16:16:39 -0500

    Update usage logic.

--- recon.sh
+++ recon.sh
@@ -15,22 +15,22 @@ BOXES="/media/sf_htb/boxes"
 if [ "$#" -ne 2 ]
 then
 	echo "Usage: . recon.sh <ip> <name>"
+else
+	echo "${GREEN}[+] Starting Nmap -sT -sV -sC Scan.${NC}"
+	nmap -v --stats-every 15 -Pn -n -sT -sV -sC --open --privileged -oA ${BOXES}/$2/scans/nmap_$2_tcp_1000 $1
+	echo "${GREEN}[+] Finished Nmap -sT -sV -sC Scan.${NC}"
+	echo "${GREEN}[+] Displaying nmap_$2_tcp_1000.nmap.${NC}"
+	cat ${BOXES}/$2/scans/nmap_$2_tcp_1000.nmap
+
+	echo "${GREEN}[+] Starting Nmap -sT -p- -sV -sC Scan.${NC}"
+	nmap -v --stats-every 15 -Pn -n -sT -p- -sV -sC --open --privileged -oA ${BOXES}/$2/scans/nmap_$2_tcp_all $1
+	echo "${GREEN}[+] Finished Nmap -sT -p- -sV -sC Scan.${NC}"
+	echo "${GREEN}[+] Displaying ndiff nmap_$2_tcp_1000.xml nmap_$2_tcp_all.xml.${NC}"
+	ndiff ${BOXES}/$2/scans/nmap_$2_tcp_1000.xml ${BOXES}/$2/scans/nmap_$2_tcp_all.xml
+
+	echo "${GREEN}[+] Starting Nmap -sU -sV -sC Scan.${NC}"
+	nmap -v --stats-every 15 -Pn -n -sU --top-ports 100 -sV -sC --open --privileged -oA ${BOXES}/$2/scans/nmap_$2_udp_100 $1
+	echo "${GREEN}[+] Finished Nmap -sU -sV -sC Scan.${NC}"
+	echo "${GREEN}[+] Displaying nmap_$2_udp_100.nmap.${NC}"
+	cat ${BOXES}/$2/scans/nmap_$2_udp_100.nmap
 fi
-
-echo "${GREEN}[+] Starting Nmap -sT -sV -sC Scan.${NC}"
-nmap -v --stats-every 15 -Pn -n -sT -sV -sC --open --privileged -oA ${BOXES}/$2/scans/nmap_$2_tcp_1000 $1
-echo "${GREEN}[+] Finished Nmap -sT -sV -sC Scan.${NC}"
-echo "${GREEN}[+] Displaying nmap_$2_tcp_1000.nmap.${NC}"
-cat ${BOXES}/$2/scans/nmap_$2_tcp_1000.nmap
-
-echo "${GREEN}[+] Starting Nmap -sT -p- -sV -sC Scan.${NC}"
-nmap -v --stats-every 15 -Pn -n -sT -p- -sV -sC --open --privileged -oA ${BOXES}/$2/scans/nmap_$2_tcp_all $1
-echo "${GREEN}[+] Finished Nmap -sT -p- -sV -sC Scan.${NC}"
-echo "${GREEN}[+] Displaying ndiff nmap_$2_tcp_1000.xml nmap_$2_tcp_all.xml.${NC}"
-ndiff ${BOXES}/$2/scans/nmap_$2_tcp_1000.xml ${BOXES}/$2/scans/nmap_$2_tcp_all.xml
-
-echo "${GREEN}[+] Starting Nmap -sU -sV -sC Scan.${NC}"
-nmap -v --stats-every 15 -Pn -n -sU --top-ports 100 -sV -sC --open --privileged -oA ${BOXES}/$2/scans/nmap_$2_udp_100 $1
-echo "${GREEN}[+] Finished Nmap -sU -sV -sC Scan.${NC}"
-echo "${GREEN}[+] Displaying nmap_$2_udp_100.nmap.${NC}"
-cat ${BOXES}/$2/scans/nmap_$2_udp_100.nmap
